4cdc2300e6f1deda0bc3a980acdead7abdcbe457
siskasofiyatunnisa <siskasofiyatunnisa.31@gmail.com>
2016-09-27 18:25:30 +0700

    Add files via upload

---
+++ siska31tkj3.ino
@@ -0,0 +1,13 @@
+void setup() {
+  //  initialize digital pin 8 as an output.
+  pinMode(8, OUTPUT)
+
+}
+
+void loop() {
+  // the loop function runs over and over again forever.
+  digitalWrite (5, HIGH) ;  // turn the LED on (HIGH is the voltage level)
+  delay(3000) ;             // wait for a second
+  digitalWrite  (5 ,LOW) ;  // turn the LED off by making the voltage LOW
+  delay(3000) ;             // wait for a second
+}
